20d0e39098d719c76aea3da4fe86430605fe54a3
MarcoGraca <marcostbgr16@gmail.com>
2018-12-10 11:01:57 +0000

    Created routes skeleton

--- server/server.py
+++ server/server.py
@@ -10,6 +10,7 @@ class User(db.Model):
     ist_ID = db.Column(db.String(9), primary_key = True)
     cur_pos_lat = db.Column(db.Float)
     cur_pos_long = db.Column(db.Float)
+    last_seen = db.Column(db.DateTime)
     #inBuilding query: SELECT buildingID from Building B, User U where sqrt((U.cur_pos_lat - B.location_lat)^2 + (U.cur_pos_lat - B.location_lat)^2) < radius
 
 class Building(db.Model):
@@ -26,7 +27,7 @@ class Stay(db.Model):
 
 class UserMessage(db.Model):
     from_istID = db.Column(db.String(9), primary_key = True)
-    sentstamp = db.Column(db.DateTime, primary_key = True)
+    sentstamp = db.Column(db.DateTime, primary_key = True) #timestamp given by server
     radius = db.Column(db.Float)
     content = db.Column(db.Text)
 
@@ -40,10 +41,74 @@ class BotMessage(db.Model):
 def hello_world():
     return 'Hello World!'
 
-@app.route('/json2')
-def ret_jsonjson():
-    return jsonify({"aaa": 12, "bbb": ["bbb", 12, 12]})
+@app.route('/user/<id>', methods=['POST']) #receives an authentication token
+#login function
+#if the user is not on the database, add him to it
+pass
 
+@app.route('/user/<id>/location', methods=['POST'])
+def update_loc(id):
+    content = request.get_json()
+    update_coord(content, id)
+    return json.dumps({'success':True}),200,{'ContentType':'application/json'}
+
+@app.route('/user/<id>/message', methods=['POST', 'GET'])
+def message_log(id):
+    if request.method == 'POST':
+        content = request.get_json()
+        post_message(content, id)
+        return json.dumps({'success':True}),200,{'ContentType':'application/json'}
+    return sent_messages()
+
+@app.route('/user/<id>/nearby/<range>', methods=['GET'])
+def update_loc(id):
+    user_table = get_users_in_range(id, range)
+    if not user_table:
+        return ('',204)
+    return json.dumps(jsonify(user_table)),200,{'ContentType':'application/json'}
+
+@app.route('/admin/users', methods=['GET'])
+def check_online_users():
+    online_table = get_users_online()
+    if not online_table:
+        return ('',204)
+    return json.dumps(jsonify(online_table)),200,{'ContentType':'application/json'}
+
+@app.route('/building', methods=['GET','POST'])
+def buildings():
+    if request.method == 'GET':
+        return json.dumps(jsonify(buildings_table)),200,{'ContentType':'application/json'}
+    content = request.get_json()
+    insert_building(content)
+    return json.dumps({'success':True}),200,{'ContentType':'application/json'}
+
+@app.route('/admin/users/<build_id>', methods=['GET'])
+def users_inside(build_id):
+    inside_table = get_users_online()
+    if not inside_table:
+        return ('',204)
+    return json.dumps(jsonify(inside_table)),200,{'ContentType':'application/json'}
+
+@app.route('/user', methods=['GET'])
+def show_users():
+    online_table = get_users_online()
+    if not online_table:
+        return ('',204)
+    return json.dumps(jsonify(online_table)),200,{'ContentType':'application/json'}
+
+@app.route('/log/user/<usr_id>', methods=['GET'])
+def user_log(usr_id):
+    all_usr_actions = get_users_online()
+    if not all_usr_actions:
+        return ('',204)
+    return json.dumps(jsonify(all_usr_actions)),200,{'ContentType':'application/json'}
+
+@app.route('/log/building/<build_id>', methods=['GET'])
+def build_log(build_id):
+    all_build_actions = get_users_online()
+    if not all_build_actions:
+        return ('',204)
+    return json.dumps(jsonify(all_build_actions)),200,{'ContentType':'application/json'}
 
 if __name__ == '__main__':
     app.run()
