4dee56778f278d68265c5f8a076bfac77962863e
MarcoGraca <marcostbgr16@gmail.com>
2018-12-12 17:17:51 +0000

    MongoDB statements

--- api/app/mod_bot/controllers.py
+++ api/app/mod_bot/controllers.py
@@ -1,15 +1,17 @@
 import json
 
 from flask import Blueprint, Flask, jsonify, request
-
+from datetime import datetime
 from app import db
 
 mod_bot = Blueprint("bot", __name__, url_prefix="/bot")
 
-
 @mod_bot.route("/register", methods=["POST"])
+def register_bot():
+    pass
+
 def check_online_users():
-    online_table = get_users_online()
+    online_table = db.User.find({'last_seen': {'$gt': datetime.utcnow() - timedelta(minutes = 10)}})
     if not online_table:
         return ("", 204)
     return json.dumps(jsonify(online_table)), 200, {
--- api/app/mod_user/controllers.py
+++ api/app/mod_user/controllers.py
@@ -1,46 +1,27 @@
 import json
 
-from flask import Blueprint, Flask, jsonify, request
-from sqlalchemy import update
-import datetime, math
+from flask import Blueprint, Flask, request
+from datetime import datetime
 
 from app import db, utils
-import app.models as models
 
 mod_user = Blueprint("user", __name__, url_prefix="/user")
 
-def update_coord(cont, user_id):
-    updt_user = db.session.query(User).filter_by(ist_ID = user_id)
-    updt_user.cur_pos_lat = cont['lat']
-    updt_user.cur_pos_long = cont['long']
-    list_buildings = db.session.query(Building).filter(utils.dist(updt_user.cur_pos_lat, Building.cur_pos_lat, updt_user.cur_pos_long, Building.cur_pos_long) < utils.default_range)
-    return
-
-def post_message(cont, user_id):
-    new_msg = models.UserMessage(from_istID = user_id, sentstamp = datetime.datetime.now(), radius = cont['radius'], content = cont['content'])
-    db.session.add(new_msg)
-    db.session.flush()
-    return
-
 def get_users_in_range(user_id, range):
-    sending_user = db.session.query(User).filter_by(ist_ID = user_id)
-    list_in_range = db.session.query(User).filter(utils.dist(User.cur_pos_lat, sending_user.cur_pos_lat, User.cur_pos_long, sending_user.cur_pos_long) < range)
+    sending_user = db.User.find_one({'ist_ID':user_id})
+    list_in_range = db.User.find({cur_pos: {$near: {$geometry: {type: 'Point', coordinates: sending_user['cur_pos']}, $maxDistance: range}}})
     return list_in_range
 
 @mod_user.route(
     "/<user_id>", methods=["GET"])  # receives an authentication token
 def register_user(user_id):
-    u = models.User(
-        ist_ID=user_id, cur_pos_lat=0, cur_pos_long=0)
-    print("user created", u)
-    db.session.add(u)
-    db.session.commit()
+    db.User.insert_one({'ist_ID':user_id, 'sentstamp': datetime.now(), 'cur_pos':[0.0,0.0]})
     return ("", 200)
 
 @mod_user.route("/<user_id>/location", methods=["POST"])
 def update_loc(user_id):
     content = request.get_json()
-    update_coord(content, user_id)
+    db.User.update_one({'ist_ID':user_id},{$set{'cur_pos':cont['cur_pos']}, $currentDate:{'last_seen':True}})
     return json.dumps({
         "success": True
     }), 200, {
@@ -52,7 +33,7 @@ def update_loc(user_id):
 def message_log(user_id):
     if request.method == "POST":
         content = request.get_json()
-        post_message(content, user_id)
+        db.UserMessage.insert_one({'from_istID': user_id, 'sentstamp': datetime.utcnow(), 'radius' = cont['radius'], 'content': cont['content']})
         return json.dumps({
             "success": True
         }), 200, {
