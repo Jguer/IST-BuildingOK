fe9ab84a1b9c31291032de63bc3384d59f33c81b
Jguer <me@jguer.space>
2018-12-14 17:09:59 +0000

    It logins surely

--- client/server.go
+++ client/server.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+var fenixKey = "***REMOVED***"
+var fenixID = "***REMOVED***"
+var fenixRedirect = "http://127.0.0.1:9090/auth"
 
 func index(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", gin.H{
@@ -12,11 +17,28 @@ func index(c *gin.Context) {
 	})
 }
 
+func auth(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"title": "Main website",
+	})
+}
+
+func login(c *gin.Context) {
+	redirectURL := fmt.Sprintf("https://fenix.tecnico.ulisboa.pt/oauth/userdialog?client_id=%s&redirect_uri=%s", fenixID, fenixRedirect)
+	c.HTML(http.StatusOK, "login.html", gin.H{
+		"RedirectURL": redirectURL,
+	})
+
+}
+
 func main() {
 	//gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	r.LoadHTMLGlob("views/*")
 
-	r.GET("/", index)
+	r.GET("/", login)
+	r.GET("/ist", index)
+	r.GET("/auth", auth)
+
 	r.Run(":9090") // listen and serve on 0.0.0.0:8080
 }
