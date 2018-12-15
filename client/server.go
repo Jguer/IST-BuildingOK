8e0e0677ca05bf108dd60ebe3eae0fc301787c5e
Jguer <me@jguer.space>
2018-12-15 09:34:22 +0000

    Finish admin auth and user auth

--- client/server.go
+++ client/server.go
@@ -1,44 +1,134 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"net/url"
 
-	"github.com/gin-gonic/gin"
+	echotemplate "github.com/foolin/echo-template"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
+type AccessToken struct {
+	Token        string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	ExpiresIn    string `json:"expires_in"`
+}
+
+type UserInfo struct {
+	Campus string `json:"campus"`
+	Roles  []struct {
+		Type          string `json:"type"`
+		Registrations []struct {
+			Name          string   `json:"name"`
+			Acronym       string   `json:"acronym"`
+			ID            string   `json:"id"`
+			AcademicTerms []string `json:"academicTerms"`
+		} `json:"registrations"`
+	} `json:"roles"`
+	Photo struct {
+		Type string `json:"type"`
+		Data string `json:"data"`
+	} `json:"photo"`
+	Name               string        `json:"name"`
+	Gender             string        `json:"gender"`
+	Birthday           string        `json:"birthday"`
+	Username           string        `json:"username"`
+	Email              string        `json:"email"`
+	DisplayName        string        `json:"displayName"`
+	InstitutionalEmail string        `json:"institutionalEmail"`
+	PersonalEmails     []string      `json:"personalEmails"`
+	WorkEmails         []interface{} `json:"workEmails"`
+	WebAddresses       []interface{} `json:"webAddresses"`
+	WorkWebAddresses   []interface{} `json:"workWebAddresses"`
+}
+
+var users map[string]UserInfo
 var fenixKey = "***REMOVED***"
 var fenixID = "***REMOVED***"
 var fenixRedirect = "http://127.0.0.1:9090/auth"
 
-func index(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H{
+func init() {
+	users = make(map[string]UserInfo)
+}
+
+func user(c echo.Context) error {
+	if _, ok := users[c.Param("id")]; !ok {
+		return c.Redirect(http.StatusFound, "/")
+	}
+
+	return c.Render(http.StatusOK, "user.html", echo.Map{
+		"user":     c.Param("id"),
+		"userInfo": users[c.Param("id")],
+	})
+}
+
+func admin(c echo.Context) error {
+	return c.Render(http.StatusOK, "admin.html", echo.Map{
 		"title": "Main website",
 	})
 }
 
-func auth(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", gin.H{
-		"title": "Main website",
-	})
+func auth(c echo.Context) error {
+	code := c.QueryParam("code")
+
+	requestURL := "https://fenix.tecnico.ulisboa.pt/oauth/access_token"
+	requestData := url.Values{
+		"client_id":     {fenixID},
+		"client_secret": {fenixKey},
+		"redirect_uri":  {fenixRedirect},
+		"code":          {code},
+		"grant_type":    {"authorization_code"},
+	}
+
+	resp, err := http.PostForm(requestURL, requestData)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	accessToken := AccessToken{}
+	json.NewDecoder(resp.Body).Decode(&accessToken)
+
+	requestURLInfo := "https://fenix.tecnico.ulisboa.pt/api/fenix/v1/person?access_token="
+	resp, err = http.Get(requestURLInfo + accessToken.Token)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	userEntry := UserInfo{}
+	json.NewDecoder(resp.Body).Decode(&userEntry)
+
+	users[userEntry.Username] = userEntry
+
+	return c.Redirect(http.StatusFound, "/user/"+userEntry.Username)
 }
 
-func login(c *gin.Context) {
+func login(c echo.Context) error {
 	redirectURL := fmt.Sprintf("https://fenix.tecnico.ulisboa.pt/oauth/userdialog?client_id=%s&redirect_uri=%s", fenixID, fenixRedirect)
-	c.HTML(http.StatusOK, "login.html", gin.H{
+	return c.Render(http.StatusOK, "login.html", echo.Map{
 		"RedirectURL": redirectURL,
 	})
 
 }
 
 func main() {
-	//gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
-	r.LoadHTMLGlob("views/*")
+	e := echo.New()
+	e.Renderer = echotemplate.Default()
 
-	r.GET("/", login)
-	r.GET("/ist", index)
-	r.GET("/auth", auth)
-
-	r.Run(":9090") // listen and serve on 0.0.0.0:8080
+	g := e.Group("/admin")
+	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "admin" && password == "123" {
+			return true, nil
+		}
+		return false, nil
+	}))
+	// u := e.Group("/user")
+	e.Static("/", "static")
+	e.GET("/", login)
+	e.GET("/user/:id", user)
+	g.GET("/console", admin)
+	e.GET("/auth", auth)
+	e.Logger.Fatal(e.Start(":9090"))
 }
